b87138c32a7415744308e8b61eef06ebcc2261d8
Geoff <geoffboyd2@gmail.com>
2022-09-01 12:19:52 -0400

    Update help.js

--- modules/help.js
+++ modules/help.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'help',
   description: 'List of commands',
   execute(bot, channel, args, from, to, commandNames) {
-    const offLimitsCommands = new Set(['randAdd', 'randInfo', 'randSelect', 'reload', 'restart']);
+    const offLimitsCommands = new Set(['getContent', 'randAdd', 'randDelete', 'randInfo', 'randSelect', 'reload', 'restart']);
     const cleansedCommands = commandNames.filter(e => !offLimitsCommands.has(e));
     bot.say(channel, `List of commands: ${cleansedCommands.join(', ')}`)
   }
